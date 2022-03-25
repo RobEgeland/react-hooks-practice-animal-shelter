0534f2fcbab7f6cd10441d94be629829d2e29e78
Robert Egeland <robegeland@yahoo.com>
2022-03-24 20:51:41 -0500

    commit tree 8b97d06379cf1b331179cab72bfb56ab48d5c90b

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,62 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Filters from "./Filters";
 import PetBrowser from "./PetBrowser";
 
 function App() {
   const [pets, setPets] = useState([]);
-  const [filters, setFilters] = useState({ type: "all" });
+  const [filters, setFilters] = useState("all");
+
+  useEffect(() => {
+    const controller = new AbortController();
+    fetch('http://localhost:3001/pets')
+    .then(res => res.json())
+    .then(petsArr => setPets(petsArr))
+    return () => {
+      controller.abort();
+    }
+  }, [])
+
+  function onChangeType(event) {
+    setFilters(event.target.value)
+  }
+
+  function onFindPetsClick() {
+    if( filters === "all") {
+      fetch('http://localhost:3001/pets')
+      .then(res => res.json())
+      .then(petsArr => setPets(petsArr))
+    }else {
+      fetch(`http://localhost:3001/pets?type=${filters}`)
+      .then(res => res.json())
+      .then(petsArr => setPets(petsArr))
+    }
+  }
+
+  function onAdoptPet(id) {
+    fetch(`http://localhost:3001/pets/${id}`, {
+      method: 'PATCH',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({"isAdopted": true})
+    })
+    .then(res => res.json())
+    .then(newPet => updatePets(newPet, id))
+  }
+
+  function updatePets(newPet, id) {
+    const newPetArr = pets.map((pet) => {
+      if(pet.id === id) {
+        return newPet
+      }else {
+        return pet
+      }
+    })
+    setPets(newPetArr)
+  }
+
+  
 
   return (
     <div className="ui container">
@@ -15,10 +66,10 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters onFindPetsClick={onFindPetsClick} onChangeType={onChangeType} />
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser onAdoptPet={onAdoptPet} pets={pets} />
           </div>
         </div>
       </div>
